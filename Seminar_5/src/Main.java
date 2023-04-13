d080573e801062d57fb68270d04fcf3126da396f
Verwolff <3angle@mail.ru>
2023-04-13 22:12:28 +0700

    Seminar 5

--- Seminar_5/src/Main.java
+++ Seminar_5/src/Main.java
@@ -3,36 +3,34 @@ import Tasks.*;
 public class Main {
     public static void main(String[] args) {
         // Task 1
-//        System.out.println(Task1.isIsomorph("foo", "bar"));
-//        System.out.println(Task1.isIsomorph("paper", "title"));
+        System.out.println(Task1.isIsomorph("foo", "bar"));
+        System.out.println(Task1.isIsomorph("paper", "title"));
 
         // Task 2
-//        System.out.println(Task2.isValid("a+(d*3)"));
-//        System.out.println(Task2.isValid("[a+(1*3)"));
-//        System.out.println(Task2.isValid("[6+(3*3)]"));
-//        System.out.println(Task2.isValid("{a}[+]{(d*3)}"));
-//        System.out.println(Task2.isValid("<{a}+{(d*3)}>"));
-//        System.out.println(Task2.isValid("{a+]}{(d*3)}"));
-//        System.out.println(Task2.isValid("{a+}{(d*3)}"));
+        System.out.println(Task2.isValid("a+(d*3)"));
+        System.out.println(Task2.isValid("[a+(1*3)"));
+        System.out.println(Task2.isValid("[6+(3*3)]"));
+        System.out.println(Task2.isValid("{a}[+]{(d*3)}"));
+        System.out.println(Task2.isValid("<{a}+{(d*3)}>"));
+        System.out.println(Task2.isValid("{a+]}{(d*3)}"));
+        System.out.println(Task2.isValid("{a+}{(d*3)}"));
         // Home task 1
-//        PhoneBook book = new PhoneBook();
-//        book.appendPhone("123", "Ivanov");
-//        book.appendPhone("456", "Petrov");
-//        book.appendPhone("147", "Kozlov");
-//        book.appendPhone("258", "Ivanov");
-//        book.appendPhone("789", "Anfreev");
-//        book.appendPhone("397", "Ivanov");
-//        book.appendPhone("159", "Ivanov");
-//        book.printAll();
-//        System.out.println(book.getPhones("Kozlov"));
-//        System.out.println(book.getPhones("Ivanov"));
+        PhoneBook book = new PhoneBook();
+        book.appendPhone("123", "Ivanov");
+        book.appendPhone("456", "Petrov");
+        book.appendPhone("147", "Kozlov");
+        book.appendPhone("258", "Ivanov");
+        book.appendPhone("789", "Anfreev");
+        book.appendPhone("397", "Ivanov");
+        book.appendPhone("159", "Ivanov");
+        book.printAll();
+        System.out.println(book.getPhones("Kozlov"));
+        System.out.println(book.getPhones("Ivanov"));
         // Home task 2
-        //HomeTask2.sortByPopularity();
+        HomeTask2.sortByPopularity();
         // HomeTask 3
         System.out.println(HomeTask3.convertRomeNumber("MMXXII"));
         System.out.println(HomeTask3.convertRomeNumber("IXX"));
         System.out.println(HomeTask3.convertRomeNumber("XII"));
-
-
     }
 }
